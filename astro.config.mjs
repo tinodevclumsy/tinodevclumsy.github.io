ee0e1c0913972855b3f2e90de551f96776da80f4
tinodevclumsy <seungjun.dev@gmail.com>
2025-03-13 21:57:42 -0700

    chore: basic project set up and install libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-   site: "https://tinodevclumsy.github.io", // GitHub Pages URL
-   base: "/", // 레포 이름 추가
-   output: "static", // GitHub Pages는 정적 사이트만 지원
+   site: "https://tinodevclumsy.github.io", 
+   base: "/", 
+   output: "static", 
+   integrations: [mdx(), sitemap()],
+   vite: {
+    plugins: [tailwindcss()],
+  },
  });
