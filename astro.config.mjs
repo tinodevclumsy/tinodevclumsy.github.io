4a1c7a0ab51337892c6c43d3c35ce1b32f46affd
tinodevclumsy <seungjun.dev@gmail.com>
2025-03-13 19:06:09 -0700

    Deploy Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   site: "https://tinodevclumsy.github.io", // GitHub Pages URL
+   base: "/", // 레포 이름 추가
+   output: "static", // GitHub Pages는 정적 사이트만 지원
+ });
