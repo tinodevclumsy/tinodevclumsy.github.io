70f07585b3458e792859c1f41136774890a14f75
tinodevclumsy <seungjun.dev@gmail.com>
2025-03-17 16:50:39 -0700

    chore: install astro icon

--- src/content.config.ts
+++ src/content.config.ts
@@ -22,7 +22,7 @@ const project = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-      technologies: z.array(z.string()),t
+      technologies: z.array(z.string()),
 		image: z.string().optional(),
       github: z.string().optional(),
       live: z.string().optional(),
