eaa870f6586e95ffb19687b084b8fbac8308245f
tinodevclumsy <seungjun.dev@gmail.com>
2025-03-18 15:33:09 -0700

    feat: seo set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import partytown from '@astrojs/partytown'
+
 export default defineConfig({
-   site: "https://tinodevclumsy.github.io", 
-   base: "/", 
-   output: "static", 
-   integrations: [mdx(), sitemap(), icon()],
-   vite: {
+  site: "https://tinodevclumsy.github.io",
+  base: "/",
+  output: "static",
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  vite: {
     plugins: [tailwindcss()],
   },
- });
+});
