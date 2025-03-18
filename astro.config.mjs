1603c5807425949f452ac2c61c1507150a1fcd6f
tinodevclumsy <seungjun.dev@gmail.com>
2025-03-17 17:23:02 -0700

    style: update style

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
-
+import icon from "astro-icon";
 export default defineConfig({
    site: "https://tinodevclumsy.github.io", 
    base: "/", 
    output: "static", 
-   integrations: [mdx(), sitemap()],
+   integrations: [mdx(), sitemap(), icon()],
    vite: {
     plugins: [tailwindcss()],
   },
